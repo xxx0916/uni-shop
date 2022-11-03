7f3b885620ea6a60a1a5cc623fe0466f175fc7c4
xxx0916 <2604350081@qq.com>
2022-11-04 00:33:59 +0800

    完成了登录和支付的功能开发

---
+++ components/my-login/my-login.vue
@@ -0,0 +1,149 @@
+<template>
+		 <view class="login-container">
+		    <!-- 提示登录的图标 -->
+		    <uni-icons type="contact-filled" size="100" color="#AFAFAF"></uni-icons>
+		    <!-- 登录按钮 -->
+		    <button type="primary" class="btn-login" @click="getUserProfile">一键登录</button>
+			<!-- <button type="primary" class="btn-login" open-type="getUserInfo" @getuserinfo="getUserInfo">一键登录</button> -->
+		    <!-- 登录提示 -->
+		    <view class="tips-text">登录后尽享更多权益</view>
+		  </view>
+</template>
+
+<script>
+	import { mapMutations,mapState } from 'vuex'
+	export default {
+		name:"my-login",
+		data() {
+			return {
+				token:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo"
+			};
+		},
+		computed:{
+			// 调用 mapState 辅助方法，把 m_user 模块中的数据映射到当前用组件中使用
+			...mapState('m_user', ['redirectInfo']),
+		},
+		methods:{
+			...mapMutations('m_user',['updateUserInfo','updateToken','updateRedirectInfo']),
+			 // 获取微信用户的基本信息
+			 async getUserProfile() {
+			 
+			 const res = await uni.getUserProfile({desc: "登录"})
+			 
+			 console.log(res[1])
+			 
+			  if (res[1]=== undefined) return uni.$showMsg('您取消了登录授权！')
+			  
+			  // 3. 将用户的基本信息存储到 vuex 中
+			  // console.log(res[1].userInfo);
+			 this.updateUserInfo(res[1].userInfo)
+			 
+			 // 获取登录成功后的 Token 字符串
+			   this.getToken(res[1])
+			 
+			 },
+			  // getUserProfile(e) {
+				 //  uni.getUserProfile({
+				 //        desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+				 //        success: (res) => {
+					// 		// console.log(res);
+				 //          // 判断是否获取用户信息成功
+				 //             if (res.errMsg === 'getUserProfile:fail auth deny') return uni.$showMsg('您取消了登录授权！')
+				          					  
+				 //             // 获取用户信息成功， res.userInfo 就是用户的基本信息
+				 //             // console.log(res.userInfo)
+					// 		 // 将用户的基本信息存储到 vuex 中
+					// 		 this.updateUserInfo(res.userInfo)
+							 
+					// 		 // 获取登录成功后的 Token 字符串
+					// 		this.getToken(res)
+				 //        }
+				 //      })   
+			  // },
+			  // 调用登录接口，换取永久的 token
+			  async getToken(info) {
+			    // 调用微信登录接口
+			    const [err, res] = await uni.login().catch(err => err)
+			    // 判断是否 uni.login() 调用失败
+			    if (err || res.errMsg !== 'login:ok') return uni.$showError('登录失败！')
+			  
+			    // 准备参数对象
+			    const query = {
+			      code: res.code,
+			      encryptedData: info.encryptedData,
+			      iv: info.iv,
+			      rawData: info.rawData,
+			      signature: info.signature
+			    }
+			  
+			    // 换取 token
+			    const { data: loginResult } = await uni.$http.post('/api/public/v1/users/wxlogin', query)
+			    // if (loginResult.meta.status !== 200) return uni.$showMsg('登录失败！')
+			     // 2. 更新 vuex 中的 token
+				 
+			    this.updateToken(this.token)
+				
+				 // 判断 vuex 中的 redirectInfo 是否为 null
+				  // 如果不为 null，则登录成功之后，需要重新导航到对应的页面
+				this.navigateBack()
+			  },
+			  // 返回登录之前的页面
+			  navigateBack(){
+				   // redirectInfo 不为 null，并且导航方式为 switchTab
+				if(this.redirectInfo&&this.redirectInfo.openType==='swichTab'){
+					// 调用小程序提供的 uni.switchTab() API 进行页面的导航
+					uni.switchTab({
+						// 要导航到的页面地址
+						url:this.redirectInfo.from,
+						// 导航成功之后，把 vuex 中的 redirectInfo 对象重置为 null
+						complete: () => {
+							this.updateRedirectInfo(null)
+						}
+					})
+				}  
+			  }
+		}
+	}
+</script>
+
+<style lang="scss">
+.login-container {
+  // 登录盒子的样式
+  height: 750rpx;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f8f8;
+  position: relative;
+  overflow: hidden;
+
+  // 绘制登录盒子底部的半椭圆造型
+  &::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 40px;
+    left: 0;
+    bottom: 0;
+    background-color: white;
+    border-radius: 100%;
+    transform: translateY(50%);
+  }
+
+  // 登录按钮的样式
+  .btn-login {
+    width: 90%;
+    border-radius: 100px;
+    margin: 15px 0;
+    background-color: #c00000;
+  }
+
+  // 按钮下方提示消息的样式
+  .tips-text {
+    font-size: 12px;
+    color: gray;
+  }
+}
+</style>
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,11 +1,16 @@
 <template>
-	<view>
-		My
+	<view class="my-container">
+		<!-- 未登录 -->
+		<my-login v-if="!token"></my-login>
+		
+		<!-- 登录后 -->
+		<my-userinfo v-else></my-userinfo>
 	</view>
 </template>
 
 <script>
 	import badgeMix from "@/mixins/tabbar-badge.js"
+	import {mapState} from "vuex"
 	export default {
 		// 将 badgeMix 混入到当前的页面中进行使用
 		mixins:[badgeMix],
@@ -13,10 +18,16 @@
 			return {
 				
 			};
+		},
+		computed:{
+			...mapState('m_user',['token'])
 		}
 	}
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
